78b51b894a108b729efff93f0b01a6313081fbea
Subasinghe <suba.11@cse.mrt.ac.lk>
2016-06-10 11:57:29 +0530

    updated with M6 release

--- samples/sample4/apps/org.wso2.uuf.sample.bpmn-explorer/pages/bpmn/{id}.js
+++ samples/sample4/apps/org.wso2.uuf.sample.bpmn-explorer/pages/bpmn/{id}.js
@@ -1,9 +1,4 @@
-/*function onRequest(context) {
-    var s = callOSGiService("org.wso2.mbp.sample01.Service", "createProcessInstance", [context.app.config.bpsHost,context.app.config.bpsPort]);
-    var a = JSON.parse(s);
-    return {"data":a};
-}*/
 var onRequest = function (context) {
     print(context);
-    return {"name": context.uriParams.id};
+    return {"name": context.pathParams['id']};
 };
